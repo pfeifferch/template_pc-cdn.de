326fc7ca5ad1b4dc4de042f06cef644f55ce38c5
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-11 08:53:16 +0200

    Add files via upload

--- assets/css/minimal-dark-mode-toggle/script-thflat.js
+++ assets/css/minimal-dark-mode-toggle/script-thflat.js
@@ -1,5 +1,10 @@
 function toggleDarkLight() {  
 var body = document.getElementById("body"); 
 var currentClass = body.className;
-body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+body.className = currentClass == "dark-mode page-template-default page" ? "light-mode page-template-default page" : "dark-mode page-template-default page";
+
+var contenttoggler = document.getElementById("contenttoggler"); 
+var currentClass = contenttoggler.className;
+contenttoggler.className = currentClass == "dark-mode entry-content page" ? "light-mode entry-content" : "dark-mode entry-content";
+
 }

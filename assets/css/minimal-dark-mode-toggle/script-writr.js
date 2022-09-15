b250e1e7b8f916c28199a50f288f5f8f064c39b5
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-15 02:31:53 +0200

    Add files via upload

--- assets/css/minimal-dark-mode-toggle/script-writr.js
+++ assets/css/minimal-dark-mode-toggle/script-writr.js
@@ -11,5 +11,10 @@ var content = document.getElementById("primary");
   var currentClass = content.className;
   content.className = currentClass == "content-area dark-mode" ? "content-area light-mode" : "content-area dark-mode";
 
+var content = document.getElementById("entry-title");
+  var currentClass = content.className;
+  content.className = currentClass == "entry-title dark-mode" ? "entry-title light-mode" : "entry-title dark-mode";
 
 }
+
+

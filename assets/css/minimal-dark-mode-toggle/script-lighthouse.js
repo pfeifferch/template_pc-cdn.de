52f9d0affba64e86d6adeec55a9d92619eff1880
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-22 19:17:06 +0200

    Add files via upload

--- assets/css/minimal-dark-mode-toggle/script-lighthouse.js
+++ assets/css/minimal-dark-mode-toggle/script-lighthouse.js
@@ -1,8 +1,9 @@
 function toggleDarkLight() {
-  var body = document.getElementById("post-2");
-  var currentClass = body.className;
-  body.className = currentClass == "post-content post-2 page type-page status-publish hentry dark-mode" ? "post-content post-2 page type-page status-publish hentry light-mode" : "post-content post-2 page type-page status-publish hentry dark-mode";
+  var post2 = document.getElementById("post-2");
+  var currentClass = post2.className;
+  post2.className = currentClass == "post-content post-2 page type-page status-publish hentry dark-mode" ? "post-content post-2 page type-page status-publish hentry light-mode" : "post-content post-2 page type-page status-publish hentry dark-mode";
 }
+
 
 
 

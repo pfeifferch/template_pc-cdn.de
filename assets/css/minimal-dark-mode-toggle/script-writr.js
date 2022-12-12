d20aeee1da2ca7d9a9fc4d2abed183f10930df6a
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-12-12 10:58:53 +0100

    Update script-writr.js

--- assets/css/minimal-dark-mode-toggle/script-writr.js
+++ assets/css/minimal-dark-mode-toggle/script-writr.js
@@ -1,3 +1,29 @@
+function toggleDarkLight() {
+
+var content = document.getElementById("page");
+  var currentClass = content.className;
+  content.className = currentClass == "hfeed site dark-mode" ? "hfeed site light-mode" : "hfeed site dark-mode";
+
+var content = document.getElementById("primary");
+  var currentClass = content.className;
+  content.className = currentClass == "content-area dark-mode" ? "content-area light-mode" : "content-area dark-mode";
+
+var content = document.getElementById("entry-title");
+  var currentClass = content.className;
+  content.className = currentClass == "entry-title dark-mode" ? "entry-title light-mode" : "entry-title dark-mode";
+
+// ISE: Alle Elemente per ClassName toggeln (da mehrere Einträge mit selber ID)
+const collection = document.getElementsByClassName("ise-entry-title");
+let numb = collection.length;
+for (i = 0; i < collection.length; i++) {
+//alert(collection[i]);
+var currentClass = collection[i].className;
+collection[i].className = currentClass == "ise-entry-title dark-mode" ? "ise-entry-title light-mode" : "ise-entry-title dark-mode"; 
+}
+
+}
+
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 /*var body = document.getElementById("body");
   var currentClass = body.className;
@@ -35,32 +61,3 @@ var content = document.getElementById("entry-title");
   content.className = currentClass == "entry-title light-mode";
 
 }
-
-
-
-function toggleDarkLight() {
-
-var content = document.getElementById("page");
-  var currentClass = content.className;
-  content.className = currentClass == "hfeed site dark-mode" ? "hfeed site light-mode" : "hfeed site dark-mode";
-
-var content = document.getElementById("primary");
-  var currentClass = content.className;
-  content.className = currentClass == "content-area dark-mode" ? "content-area light-mode" : "content-area dark-mode";
-
-var content = document.getElementById("entry-title");
-  var currentClass = content.className;
-  content.className = currentClass == "entry-title dark-mode" ? "entry-title light-mode" : "entry-title dark-mode";
-
-// ISE: Alle Elemente per ClassName toggeln (da mehrere Einträge mit selber ID)
-const collection = document.getElementsByClassName("ise-entry-title");
-let numb = collection.length;
-for (i = 0; i < collection.length; i++) {
-//alert(collection[i]);
-var currentClass = collection[i].className;
-collection[i].className = currentClass == "ise-entry-title dark-mode" ? "ise-entry-title light-mode" : "ise-entry-title dark-mode"; 
-}
-
-}
-
-

2ba3c529a0e69879bd32d9518fea1b419c8edd88
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-12-12 10:45:23 +0100

    Update script.js

--- assets/css/minimal-dark-mode-toggle/script.js
+++ assets/css/minimal-dark-mode-toggle/script.js
@@ -1,3 +1,13 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode";
+} else {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "light-mode";
+}
+
 function toggleDarkLight() {
   var body = document.getElementById("body");
   var currentClass = body.className;

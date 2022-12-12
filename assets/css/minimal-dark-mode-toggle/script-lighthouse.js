6f94d44f654377efa9cf989b79b156b6a04b0226
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-12-12 10:47:51 +0100

    Update script-lighthouse.js

--- assets/css/minimal-dark-mode-toggle/script-lighthouse.js
+++ assets/css/minimal-dark-mode-toggle/script-lighthouse.js
@@ -1,3 +1,13 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  var post2 = document.getElementById("post-2");
+  var currentClass = post2.className;
+  post2.className = currentClass == "post-content post-2 page type-page status-publish hentry dark-mode";
+} else {
+  var post2 = document.getElementById("post-2");
+  var currentClass = post2.className;
+  post2.className = currentClass == "post-content post-2 page type-page status-publish hentry light-mode";
+}
+
 function toggleDarkLight() {
   var post2 = document.getElementById("post-2");
   var currentClass = post2.className;

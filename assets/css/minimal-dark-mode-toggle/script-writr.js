13673dfdcfd746f68a5e2e7d6a9998ce497f7287
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-12-12 10:53:51 +0100

    Update script-writr.js

--- assets/css/minimal-dark-mode-toggle/script-writr.js
+++ assets/css/minimal-dark-mode-toggle/script-writr.js
@@ -1,9 +1,45 @@
-function toggleDarkLight() {
-/*
-  var body = document.getElementById("body");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+/*var body = document.getElementById("body");
   var currentClass = body.className;
-  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  body.className = currentClass == "dark-mode";
 */
+
+var content = document.getElementById("page");
+  var currentClass = content.className;
+  content.className = currentClass == "hfeed site dark-mode";
+
+var content = document.getElementById("primary");
+  var currentClass = content.className;
+  content.className = currentClass == "content-area dark-mode";
+
+var content = document.getElementById("entry-title");
+  var currentClass = content.className;
+  content.className = currentClass == "entry-title dark-mode";
+
+} else {
+/*var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "light-mode";
+*/
+
+var content = document.getElementById("page");
+  var currentClass = content.className;
+  content.className = currentClass == "hfeed site light-mode";
+
+var content = document.getElementById("primary");
+  var currentClass = content.className;
+  content.className = currentClass == "content-area light-mode";
+
+var content = document.getElementById("entry-title");
+  var currentClass = content.className;
+  content.className = currentClass == "entry-title light-mode";
+
+}
+
+
+
+function toggleDarkLight() {
+
 var content = document.getElementById("page");
   var currentClass = content.className;
   content.className = currentClass == "hfeed site dark-mode" ? "hfeed site light-mode" : "hfeed site dark-mode";
@@ -16,7 +52,6 @@ var content = document.getElementById("entry-title");
   var currentClass = content.className;
   content.className = currentClass == "entry-title dark-mode" ? "entry-title light-mode" : "entry-title dark-mode";
 
-
 // ISE: Alle Elemente per ClassName toggeln (da mehrere Einträge mit selber ID)
 const collection = document.getElementsByClassName("ise-entry-title");
 let numb = collection.length;
@@ -25,8 +60,6 @@ for (i = 0; i < collection.length; i++) {
 var currentClass = collection[i].className;
 collection[i].className = currentClass == "ise-entry-title dark-mode" ? "ise-entry-title light-mode" : "ise-entry-title dark-mode"; 
 }
-
-
 
 }
 
